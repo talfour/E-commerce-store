ce8a3a426d262c532b8d707afcf6bca078d411b2
Jakub <jak.malinowski94@gmail.com>
2023-05-08 17:17:33 +0200

    added models

--- backend/core/models.py
+++ backend/core/models.py
@@ -1,3 +1,101 @@
-from django.db import models
+import uuid
+import os
 
-# Create your models here.
+from datetime import date
+
+from django.db import models
+from mptt.models import MPTTModel, TreeForeignKey
+
+
+class Category(MPTTModel):
+    """Model for product category"""
+
+    name = models.CharField(max_length=125)
+    parent = TreeForeignKey("self", on_delete=models.PROTECT, null=True, blank=True)
+
+    class MPTTMeta:
+        order_insertion_by = ["name"]
+
+    def __str__(self):
+        return self.name
+
+
+class Brand(models.Model):
+    """Model for product brand"""
+
+    name = models.CharField(max_length=125)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    """Model representing Product"""
+
+    name = models.CharField(max_length=125)
+    description = models.TextField(blank=True)
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
+    category = TreeForeignKey(
+        "Category", null=True, blank=True, on_delete=models.SET_NULL
+    )
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    available = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ("name",)
+
+    def __str__(self):
+        return self.name
+
+
+def upload_image(instance, filename):
+    """Generate a UUID for the image filename."""
+    ext = filename.split(".")[-1]
+    filename = f"{uuid.uuid4()}.{ext}"
+    today = date.today().strftime("%Y/%m/%d")
+    return os.path.join("products", today, filename)
+
+
+class ProductImages(models.Model):
+    """Model representing product images"""
+
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to=upload_image, blank=True)
+
+
+class Order(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField()
+    address = models.CharField(max_length=250)
+    postal_code = models.CharField(max_length=20)
+    city = models.CharField(max_length=100)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    paid = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ("-created",)
+
+    def __str__(self):
+        return "Order {}".format(self.id)
+
+    def get_total_cost(self):
+        return sum(item.get_cost() for item in self.items.all())
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, related_name="items", on_delete=models.CASCADE)
+    product = models.ForeignKey(
+        Product, related_name="order_items", on_delete=models.CASCADE
+    )
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return "{}".format(self.id)
+
+    def get_cost(self):
+        return self.price * self.quantity
