b9236c2ef263f289d42e6c687f4ed23c2ba1c3f3
Jakub <jak.malinowski94@gmail.com>
2023-05-09 11:02:15 +0200

    added admin interface

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,3 +1,49 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Brand, Category, Product, ProductImages, Order, OrderItem
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ["name"]
+
+
+@admin.register(Brand)
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ["name"]
+
+
+class ProductImagesInline(admin.StackedInline):
+    model = ProductImages
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ["name", "price", "available", "created", "updated"]
+    list_filter = ["available", "created", "updated"]
+    list_editable = ["price", "available"]
+    inlines = [ProductImagesInline]
+
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    raw_id_fields = ["product"]
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = [
+        "id",
+        "first_name",
+        "last_name",
+        "email",
+        "address",
+        "postal_code",
+        "city",
+        "paid",
+        "created",
+        "updated",
+    ]
+    list_filter = ["paid", "created", "updated"]
+    inlines = [OrderItemInline]
