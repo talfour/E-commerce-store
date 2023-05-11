1509672d17d674e64ce29af4ce629dfe98d1206e
Jakub <jak.malinowski94@gmail.com>
2023-05-11 10:24:39 +0200

    image serializer added

--- backend/product/serializers.py
+++ backend/product/serializers.py
@@ -7,6 +7,7 @@ class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Category
         fields = [
+            "id",
             "name",
         ]
 
@@ -18,11 +19,15 @@ class BrandSerializer(serializers.ModelSerializer):
             "name",
         ]
 
+class ProductImagesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.ProductImages
+        fields = ['id', 'image',]
 
 class ProductSerializer(serializers.ModelSerializer):
+    images = ProductImagesSerializer(many=True)
     brand = BrandSerializer()
-    category = CategorySerializer()
 
     class Meta:
         model = models.Product
-        fields = "__all__"
+        fields = ['id', 'name', 'description', 'brand', 'category', 'price', 'available', 'created', 'updated', 'images']
