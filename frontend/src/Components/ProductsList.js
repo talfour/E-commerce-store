dd685ac6627fd50b59778d1e565820df78b65c4a
Jakub <jak.malinowski94@gmail.com>
2023-05-11 10:28:33 +0200

    List of products component added

---
+++ frontend/src/Components/ProductsList.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from "react";
+import { axiosInstance } from "../axios";
+import Product from "./Product";
+
+const ProductsList = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const getData = async () => {
+    try {
+      const response = await axiosInstance.get("product/");
+      setProducts(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <>
+      <h1 className="text-center text-5xl pt-5 pb-10 font-roboto font-bold">
+        Dostępne produkty:
+      </h1>
+
+      <div className="flex items-center p-4 gap-4 flex-wrap align-middle justify-center">
+        {products.map((product) => (
+          <Product key={product.id} product={product} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default ProductsList;
