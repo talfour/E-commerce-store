a2f9e1572cf0aaaa63348b59be34da4aa553de6f
Jakub <jak.malinowski94@gmail.com>
2023-06-12 11:36:51 +0200

    fixed link to product

--- frontend/src/Components/Product.js
+++ frontend/src/Components/Product.js
@@ -19,7 +19,7 @@ const Product = ({ product }) => {
   };
   return product.available ? (
     <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-      <Link to={`product/${product.id}`}>
+      <Link to={`/product/${product.id}`}>
         <img
           className="p-8 rounded-t-lg object-cover h-48 w-96"
           src={productImage}
@@ -27,7 +27,7 @@ const Product = ({ product }) => {
         />
       </Link>
       <div className="px-5 pb-5">
-        <Link to={`product/${product.id}`}>
+        <Link to={`/product/${product.id}`}>
           <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
             {product.name}
           </h5>
