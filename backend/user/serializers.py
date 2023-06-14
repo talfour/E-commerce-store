da42555f883f0cb2368c8ed3c3439d552e25fdf5
Jakub <jak.malinowski94@gmail.com>
2023-06-14 15:07:10 +0200

    update function and refactor

--- backend/user/serializers.py
+++ backend/user/serializers.py
@@ -6,34 +6,48 @@ from django.contrib.auth import get_user_model, authenticate
 from rest_framework import serializers
 
 
-class UserRegisterSerializer(serializers.ModelSerializer):
-    """Serializer for user registration."""
+class UserSerializer(serializers.ModelSerializer):
+    """Serializer for the user object."""
 
     class Meta:
         model = get_user_model()
-        fields = ['email', 'password', 'name']
-        extra_kwargs = {'password': {'write_only': True, 'min_length': 8}}
+        fields = ["email", "password", "name"]
+        extra_kwargs = {"password": {"write_only": True, "min_length": 8}}
 
     def create(self, validated_data):
         """Create and return a user with encrypted password."""
-        return get_user_model().objects.create_user(**validated_data)
+        user_obj = get_user_model().objects.create_user(**validated_data)
+        return user_obj
+
+    def update(self, instance, validated_data):
+        """Update and return user."""
+        password = validated_data.pop("password", None)
+        user = super().update(instance, validated_data)
+
+        if password:
+            try:
+                user.set_password(password)
+                user.save()
+            except Exception as e:
+                raise Exception(e)
+
+        return user
 
 
 class UserLoginSerializer(serializers.Serializer):
     """Serializer for user login."""
+
     email = serializers.EmailField()
     password = serializers.CharField()
 
     def check_user(self, validated_data):
-        user = authenticate(email=validated_data['email'], password=validated_data['password'])
-
+        email = validated_data.get("email")
+        password = validated_data.get("password")
+        if not email:
+            raise serializers.ValidationError("Email is required to login.")
+        if not password:
+            raise serializers.ValidationError("Password is required to login.")
+        user = authenticate(email=email, password=password)
         if not user:
-            raise serializers.ValidationError('User not found.')
+            raise serializers.ValidationError("User not found.")
         return user
-
-
-class UserSerializer(serializers.ModelSerializer):
-    """Serializer for the user object."""
-    class Meta:
-        model = get_user_model()
-        fields = ['email', 'name']
