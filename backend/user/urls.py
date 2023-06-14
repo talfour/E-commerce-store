f01cce6403dfaa747a39b76bbf1ca9deea89d88a
Jakub <jak.malinowski94@gmail.com>
2023-06-14 13:05:54 +0200

    urls for user

---
+++ backend/user/urls.py
@@ -0,0 +1,15 @@
+"""
+URL mappings for the user API.
+"""
+from django.urls import path
+
+from user import views
+
+app_name = 'user'
+
+urlpatterns = [
+    path('register/', views.UserRegister.as_view(), name='register'),
+    path('login/', views.UserLogin.as_view(), name='login'),
+    path('logout/', views.UserLogout.as_view(), name='logout'),
+    path('me/', views.UserView.as_view(), name='me'),
+]
