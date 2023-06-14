839f838cd715b7eee32a32620c6f8ba312ec0f5c
Jakub <jak.malinowski94@gmail.com>
2023-06-14 13:06:13 +0200

    views for user

--- backend/user/views.py
+++ backend/user/views.py
@@ -1,3 +1,89 @@
-from django.shortcuts import render
+"""
+Views for the user API.
+"""
+from django.contrib.auth import login, logout
 
-# Create your views here.
+from drf_spectacular.utils import extend_schema
+
+from rest_framework import permissions, status
+from rest_framework.authentication import SessionAuthentication
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+
+from .serializers import UserLoginSerializer, UserRegisterSerializer, UserSerializer
+
+
+
+class UserRegister(APIView):
+    """View for registering new user in the system."""
+    permission_classes = (permissions.AllowAny,)
+
+    @extend_schema(
+        request=None,
+        operation_id='Register User',
+        responses={
+            200: UserRegisterSerializer
+        },
+        tags=['User'])
+    def post(self, request):
+        serializer = UserRegisterSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            user = serializer.create(request.data)
+            if user:
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+class UserLogin(APIView):
+    """View for logging user"""
+    permission_classes = (permissions.AllowAny,)
+    authentication_classes = (SessionAuthentication,)
+
+
+    @extend_schema(
+        request=None,
+        operation_id='Login User',
+        responses={
+            200: UserLoginSerializer
+        },
+        tags=['User'])
+    def post(self, request):
+        data = request.data
+        serializer = UserLoginSerializer(data=data)
+        if serializer.is_valid(raise_exception=True):
+            user = serializer.check_user(data)
+            login(request, user)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class UserView(APIView):
+    """View for showing user data."""
+    permission_classes = (permissions.IsAuthenticated,)
+    authentication_classes = (SessionAuthentication,)
+
+    @extend_schema(
+        request=None,
+        operation_id='Shows User info.',
+        responses={
+            200: UserSerializer
+        },
+        tags=['User'])
+    def get(self, request):
+        serializer = UserSerializer(request.user)
+        return Response({'user': serializer.data}, status=status.HTTP_200_OK)
+
+
+class UserLogout(APIView):
+    """View for logging user out from the system."""
+
+    @extend_schema(
+        request=None,
+        operation_id='Logout User',
+        responses={
+            200: UserSerializer
+        },
+        tags=['User'])
+    def post(self, request):
+        logout(request)
+        return Response(status=status.HTTP_200_OK)
