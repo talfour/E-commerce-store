535fb48405d1a22c25d97d2d73420402a5487083
Jakub <jak.malinowski94@gmail.com>
2023-06-14 13:04:23 +0200

    user url added

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -20,6 +20,7 @@ urlpatterns = [
     path("api/", include(router.urls)),
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path("api/schema/docs", SpectacularSwaggerView.as_view(url_name="schema")),
+    path("api/user/", include('user.urls'))
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
