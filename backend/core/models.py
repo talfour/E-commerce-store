aa992cbec0f259194355a394c85b40e5fe6bcc95
Jakub <jak.malinowski94@gmail.com>
2023-06-14 13:04:53 +0200

    added user

--- backend/core/models.py
+++ backend/core/models.py
@@ -122,16 +122,30 @@ class OrderItem(models.Model):
 
 class UserManager(BaseUserManager):
     """Manager for users."""
+
     def create_user(self, email, password=None, **extra_field):
         """Create, save and return a new user."""
+        if not email:
+            raise ValueError('User must have an email address.')
         user = self.model(email=self.normalize_email(email), **extra_field)
         user.set_password(password)
         user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password):
+        """Create, save and return a new superuser"""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save()
+
         return user
 
 
 class User(AbstractBaseUser, PermissionsMixin):
     """Model representing user in the system."""
+
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
