12ecb23ffcb90dd0244db8e6117aa0eadf12ab59
Jakub <jak.malinowski94@gmail.com>
2023-06-14 13:04:46 +0200

    defined admin page for users

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
-from .models import Brand, Category, Product, ProductImages, Order, OrderItem
+from .models import Brand, Category, Product, ProductImages, Order, OrderItem, User
 
 
 @admin.register(Category)
@@ -47,3 +48,38 @@ class OrderAdmin(admin.ModelAdmin):
     ]
     list_filter = ["paid", "created", "updated"]
     inlines = [OrderItemInline]
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    """Define the admin pages for users."""
+    ordering = ['id']
+    list_display = ['email', 'name']
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (
+            _('Permissions'),
+            {
+                'fields': (
+                    'is_active',
+                    'is_staff',
+                    'is_superuser',
+                )
+            }
+        ),
+        (_('Important dates'), {'fields': ('last_login',)})
+    )
+    readonly_fields = ['last_login']
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': (
+                'email',
+                'password1',
+                'password2',
+                'name',
+                'is_active',
+                'is_staff',
+                'is_superuser',
+            )
+        }),
+    )
