2f9b704d6ebe97b19734a6b6b00c94f387beb4d8
Jakub <jak.malinowski94@gmail.com>
2023-06-16 12:34:34 +0200

    tests for cart

---
+++ backend/cart/tests/test_cart_api.py
@@ -0,0 +1,72 @@
+from django.test import TestCase
+from rest_framework.test import APIClient
+from rest_framework.reverse import reverse
+from rest_framework import status
+from core.models import Product, Category, Brand
+from cart.cart import Cart
+
+
+CART_URL_LIST = reverse("cart-list")
+CART_URL_LIST_DATA = reverse("cart-get_cart_data")
+
+
+class CartViewTests(TestCase):
+    """Test the features of shopping cart."""
+
+    def setUp(self):
+        self.client = APIClient()
+        category = Category.objects.create(name="Test category")
+        brand = Brand.objects.create(name="Test brand")
+        self.product1 = Product.objects.create(
+            name="Product 1",
+            category=category,
+            brand=brand,
+            description="Product 1 desc",
+            price=10.0,
+            available=True,
+        )
+        self.product2 = Product.objects.create(
+            name="Product 2",
+            category=category,
+            brand=brand,
+            description="Product 2 desc",
+            price=20.0,
+            available=True,
+        )
+
+    def test_cart_get_urls(self):
+        """Test that cart URLs are working"""
+        response = self.client.get(CART_URL_LIST)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIsInstance(response.data, list)
+        response = self.client.get(CART_URL_LIST_DATA)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIsInstance(response.data, dict)
+
+    def test_add_items_to_cart(self):
+        """Test that items added to the cart are successful."""
+        response = self.client.get(CART_URL_LIST_DATA)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIsInstance(response.data, dict)
+        self.assertEqual(len(response.data["cart_items"]), 0)
+
+        self.client.post(reverse("cart-add", args=[self.product1.id]), {"quantity": 1})
+        self.client.post(reverse("cart-add", args=[self.product2.id]), {"quantity": 2})
+
+        response = self.client.get(CART_URL_LIST_DATA)
+        self.assertEqual(len(response.data), 2)
+        self.assertEqual(response.data["cart_items"][0]["name"], self.product1.name)
+
+    def test_remove_item_from_cart(self):
+        """Test that item is removed from cart successful."""
+        self.client.post(reverse("cart-add", args=[self.product1.id]), {"quantity": 1})
+        self.client.post(reverse("cart-add", args=[self.product2.id]), {"quantity": 10})
+        response = self.client.get(CART_URL_LIST_DATA)
+        self.assertEqual(len(response.data["cart_items"]), 2)
+
+        url = reverse("cart-remove", args=[self.product1.id])
+        response = self.client.delete(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        new_response = self.client.get(CART_URL_LIST_DATA)
+        self.assertEqual(len(new_response.data["cart_items"]), 1)
---
+++ backend/cart/tests/test_cart_session.py
@@ -0,0 +1,87 @@
+from decimal import Decimal
+from django.test import TestCase
+from django.test.client import RequestFactory
+from django.contrib.sessions.middleware import SessionMiddleware
+from core.models import Product, Category, Brand
+from cart.cart import Cart
+
+
+class CartTestCase(TestCase):
+    """Test the features of shopping cart stored in session."""
+
+    def setUp(self):
+        self.factory = RequestFactory()
+        self.request = self.factory.get("/")
+        self.session_middleware = SessionMiddleware(lambda request: None)
+        self.session_middleware.process_request(self.request)
+        self.request.session.save()
+        category = Category.objects.create(name="Test category")
+        brand = Brand.objects.create(name="Test brand")
+        self.product1 = Product.objects.create(
+            name="Product 1",
+            category=category,
+            brand=brand,
+            description="Product 1 desc",
+            price=10.0,
+            available=True,
+        )
+        self.product2 = Product.objects.create(
+            name="Product 2",
+            category=category,
+            brand=brand,
+            description="Product 2 desc",
+            price=20.0,
+            available=True,
+        )
+
+    def test_cart_initialization(self):
+        """Test that checks if cart was initialized."""
+        cart = Cart(self.request)
+        self.assertIsInstance(cart.cart, dict)
+
+    def test_cart_iteration(self):
+        """Test iterate the cart object."""
+        cart = Cart(self.request)
+        cart.add(self.product1, quantity=2)
+        for item in cart:
+            self.assertEqual(item["product"], self.product1)
+            self.assertEqual(item["quantity"], 2)
+            self.assertEqual(item["price"], Decimal(self.product1.price))
+            self.assertEqual(item["total_price"], Decimal("20.0"))
+
+    def test_cart_length(self):
+        """Test that checks if cart length is correct."""
+        cart = Cart(self.request)
+        cart.add(self.product1, quantity=2)
+        self.assertEqual(len(cart), 2)
+
+    def test_cart_add(self):
+        """Test item add successfully."""
+        cart = Cart(self.request)
+        cart.add(self.product1, quantity=2)
+        self.assertEqual(cart.cart[str(self.product1.id)]["quantity"], 2)
+        cart.add(self.product1, quantity=3)
+        self.assertEqual(cart.cart[str(self.product1.id)]["quantity"], 5)
+
+    def test_cart_remove(self):
+        """Test item removed from cart successfully."""
+        cart = Cart(self.request)
+        cart.add(self.product1, quantity=2)
+        self.assertIn(str(self.product1.id), cart.cart)
+        cart.remove(self.product1)
+        self.assertNotIn(str(self.product1.id), cart.cart)
+
+    def test_cart_get_total_price(self):
+        """Test check if total prices is correct after adding objects to cart."""
+        cart = Cart(self.request)
+        cart.add(self.product1, quantity=2)
+        cart.add(self.product2, quantity=3)
+        self.assertEqual(cart.get_total_price(), Decimal("80.0"))
+
+    def test_cart_clear(self):
+        """Test clearing cart."""
+        cart = Cart(self.request)
+        cart.add(self.product1, quantity=2)
+        self.assertIn(str(self.product1.id), cart.cart)
+        cart.clear()
+        self.assertNotIn(str(self.product1.id), cart.cart)
