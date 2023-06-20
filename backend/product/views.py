fc6df5414f6b5806ff678c64dace046ea5ca9beb
Jakub <jak.malinowski94@gmail.com>
2023-06-20 13:23:31 +0200

    added permissions and fixed queryset

--- backend/product/views.py
+++ backend/product/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import get_object_or_404
 
 from rest_framework import viewsets
 from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
 from drf_spectacular.utils import extend_schema
 
 from core import models
@@ -11,12 +12,16 @@ from .serializers import CategorySerializer, BrandSerializer, ProductSerializer
 class CategoryViewSet(viewsets.ViewSet):
     """Simple Viewset for viewing categories."""
 
-    queryset = models.Category.objects.all()
+    permission_classes = [
+        AllowAny,
+    ]
+
     serializer_class = CategorySerializer()
 
     @extend_schema(responses=CategorySerializer)
     def list(self, request):
-        serializer = CategorySerializer(self.queryset, many=True)
+        queryset = models.Category.objects.all()
+        serializer = CategorySerializer(queryset, many=True)
         return Response(serializer.data)
 
     def retrieve(self, request, pk=None):
@@ -28,34 +33,46 @@ class CategoryViewSet(viewsets.ViewSet):
         category_data = serializer.data
 
         products = models.Product.objects.filter(category=category)
-        product_serializer = ProductSerializer(products, many=True, context={"request":request})
-        category_data['products'] = product_serializer.data
+        product_serializer = ProductSerializer(
+            products, many=True, context={"request": request}
+        )
+        category_data["products"] = product_serializer.data
         return Response(category_data)
 
 
 class BrandViewSet(viewsets.ViewSet):
     """Simple Viewset for viewing brands."""
 
-    queryset = models.Brand.objects.all()
+    permission_classes = [
+        AllowAny,
+    ]
+
 
     @extend_schema(responses=BrandSerializer)
     def list(self, request):
-        serializer = BrandSerializer(self.queryset, many=True)
+        queryset = models.Brand.objects.all()
+        serializer = BrandSerializer(queryset, many=True)
         return Response(serializer.data)
 
 
 class ProductViewSet(viewsets.ViewSet):
     """Simple Viewset for viewing all products."""
 
-    queryset = models.Product.objects.all()
+    permission_classes = [
+        AllowAny,
+    ]
 
     @extend_schema(responses=ProductSerializer)
     def list(self, request):
-        serializer = ProductSerializer(self.queryset, many=True, context={"request": request})
+        queryset = models.Product.objects.all()
+        serializer = ProductSerializer(
+            queryset, many=True, context={"request": request}
+        )
         return Response(serializer.data)
 
     @extend_schema(responses=ProductSerializer)
     def retrieve(self, request, pk=None):
-        product = get_object_or_404(self.queryset, pk=pk)
+        queryset = models.Product.objects.all()
+        product = get_object_or_404(queryset, pk=pk)
         serializer = ProductSerializer(product, context={"request": request})
         return Response(serializer.data)
