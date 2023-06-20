0391373eb67d2e98324345de4b78ac698f78c57d
Jakub <jak.malinowski94@gmail.com>
2023-06-20 15:26:38 +0200

    close button icon changed

--- frontend/src/Components/Navbar.js
+++ frontend/src/Components/Navbar.js
@@ -108,26 +108,47 @@ const Navbar = () => {
                 <span className="relative inline-flex rounded-full h-3 w-3 bg-pink-500"></span>
               </span>
             </Link>
-            <div
-              to="/profile"
-              className="navbar-burger self-center mr-12 xl:hidden cursor-pointer"
-              onClick={toggleSidebar}
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-6 w-6 hover:text-gray-200"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
+            {isOpen ? (
+              <div
+                className="navbar-burger self-center mr-12 xl:hidden cursor-pointer"
+                onClick={toggleSidebar}
               >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h16M4 18h16"
-                />
-              </svg>
-            </div>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              </div>
+            ) : (
+              <div
+                className="navbar-burger self-center mr-12 xl:hidden cursor-pointer"
+                onClick={toggleSidebar}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6 hover:text-gray-200"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h16M4 18h16"
+                  />
+                </svg>
+              </div>
+            )}
           </nav>
         </section>
       </div>
