34e7d836ba10ed4a477ff7b5b7cee6e3afd8180b
Jakub <jak.malinowski94@gmail.com>
2023-06-27 15:00:46 +0200

    added search bar

--- frontend/src/Components/ProductsList.js
+++ frontend/src/Components/ProductsList.js
@@ -20,10 +20,29 @@ const ProductsList = () => {
 
   return (
     <main>
-      <h1 className="text-center text-5xl pt-10 pb-10 font-roboto font-bold">
+      <h1 className="text-center lg:text-5xl text-3xl pt-10 pb-10 font-roboto font-bold">
         Dostępne produkty:
       </h1>
+      <div class="my-3 lg:w-4/6 w-11/12 mx-auto">
+        <div class="relative mb-4 flex w-full flex-wrap items-stretch">
+          <input
+            type="search"
+            class="relative text-center bg-white text-black m-0 -mr-0.5 block w-[1px] min-w-0 flex-auto rounded-l border border-solid border-neutral-300 bg-clip-padding px-3 py-[0.25rem]  font-normal leading-[1.6] outline-none transition duration-200 ease-in-out focus:z-[3] focus:border-primary focus:text-black focus:shadow-[inset_0_0_0_1px_rgb(59,113,202)] focus:outline-none dark:border-neutral-600"
+            placeholder="Szukaj produktów..."
+            aria-label="Search"
+            aria-describedby="button-addon3"
+          />
 
+          <button
+            class="text-white bg-blue-700 hover:bg-blue-800 relative z-[2] rounded-r border-1 border-primary px-6 py-2 text-xs font-medium uppercase text-primary transition duration-150 ease-in-out focus:outline-none focus:ring-0"
+            type="button"
+            id="button-addon3"
+            data-te-ripple-init
+          >
+            Szukaj
+          </button>
+        </div>
+      </div>
       <div className="flex items-center p-4 gap-4 flex-wrap align-middle justify-center">
         {products.map((product) => (
           <Product key={product.id} product={product} />
