20cc4322361fa505c351827a75f19fad22eed694
Jakub <jak.malinowski94@gmail.com>
2023-06-29 18:17:31 +0200

    translation

--- backend/core/tasks.py
+++ backend/core/tasks.py
@@ -10,6 +10,6 @@ def order_created(order_id):
     subject = f"Numer zamówienia: {order.id}"
     message = f"Witaj {order.first_name}! Informujemy Cię, że otrzymaliśmy twoje zamówienie\
         i jest ono w trakcie realizacji.\
-        order ID number is: {order.id}"
+        numer zamówienia: {order.id}"
     mail_sent = send_mail(subject, message, "no-reply@vapemate.com", [order.email])
     return mail_sent
