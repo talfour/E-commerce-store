99f86fc692a2d70287f3a851526e1f6f8432d7d6
Jakub <jak.malinowski94@gmail.com>
2023-06-29 18:17:04 +0200

    lint fix

--- backend/core/tasks.py
+++ backend/core/tasks.py
@@ -8,7 +8,8 @@ def order_created(order_id):
     """Task that send notification via email after creating a new order."""
     order = Order.objects.get(id=order_id)
     subject = f"Numer zamówienia: {order.id}"
-    message = f"Witaj {order.first_name}! Informujemy Cię, że otrzymaliśmy twoje zamówienie i jest ono w trakcie realizacji.\
+    message = f"Witaj {order.first_name}! Informujemy Cię, że otrzymaliśmy twoje zamówienie\
+        i jest ono w trakcie realizacji.\
         order ID number is: {order.id}"
     mail_sent = send_mail(subject, message, "no-reply@vapemate.com", [order.email])
     return mail_sent
