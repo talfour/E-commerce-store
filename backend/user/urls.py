a6af12c412088cdc22eb01871b3b015a4a46652d
Jakub <jak.malinowski94@gmail.com>
2023-06-29 17:52:26 +0200

    added urls for addresses

--- backend/user/urls.py
+++ backend/user/urls.py
@@ -4,11 +4,23 @@ URL mappings for the user API.
 from django.urls import path
 from user import views
 
-app_name = "user"
+app_name = "User"
 
 urlpatterns = [
     path("register/", views.UserRegister.as_view(), name="register"),
     path("login/", views.UserLogin.as_view(), name="login"),
     path("logout/", views.UserLogout.as_view(), name="logout"),
     path("me/", views.UserView.as_view(), name="me"),
+    path(
+        "address/",
+        views.UserAddressViewSet.as_view({"get": "list", "post": "create"}),
+        name="address",
+    ),
+    path(
+        "address/<int:pk>/",
+        views.UserAddressViewSet.as_view(
+            {"get": "retrieve", "patch": "partial_update"}
+        ),
+        name="address-detail",
+    ),
 ]
