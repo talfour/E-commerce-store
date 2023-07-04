5b8e5e2a12f963072c4e3eb6036720642e69c229
Jakub <jak.malinowski94@gmail.com>
2023-07-04 14:14:53 +0200

    renamed class attr

--- frontend/src/Components/ProductsList.js
+++ frontend/src/Components/ProductsList.js
@@ -44,11 +44,11 @@ const ProductsList = () => {
       <h1 className="text-center lg:text-5xl text-3xl pt-10 pb-10 font-roboto font-bold">
         Dostępne produkty:
       </h1>
-      <div class="my-3 lg:w-6/12 w-11/12 mx-auto">
-        <div class="relative mb-4 flex w-full flex-wrap items-stretch">
+      <div className="my-3 lg:w-6/12 w-11/12 mx-auto">
+        <div className="relative mb-4 flex w-full flex-wrap items-stretch">
           <input
             type="search"
-            class="relative text-center bg-white text-black m-0 -mr-0.5 block w-[1px] min-w-0 flex-auto rounded-l border border-solid border-neutral-300 bg-clip-padding px-3 py-[0.25rem]  font-normal leading-[1.6] outline-none transition duration-200 ease-in-out focus:z-[3] focus:border-primary focus:text-black focus:shadow-[inset_0_0_0_1px_rgb(59,113,202)] focus:outline-none"
+            className="relative text-center bg-white text-black m-0 -mr-0.5 block w-[1px] min-w-0 flex-auto rounded-l border border-solid border-neutral-300 bg-clip-padding px-3 py-[0.25rem]  font-normal leading-[1.6] outline-none transition duration-200 ease-in-out focus:z-[3] focus:border-primary focus:text-black focus:shadow-[inset_0_0_0_1px_rgb(59,113,202)] focus:outline-none"
             placeholder="Szukaj produktów..."
             aria-label="Search"
             aria-describedby="button-addon3"
@@ -56,7 +56,7 @@ const ProductsList = () => {
           />
 
           <button
-            class="text-white bg-blue-700 hover:bg-blue-800 relative z-[2] rounded-r border-1 border-primary px-6 py-2 text-xs font-medium uppercase text-primary transition duration-150 ease-in-out focus:outline-none focus:ring-0"
+            className="text-white bg-blue-700 hover:bg-blue-800 relative z-[2] rounded-r border-1 border-primary px-6 py-2 text-xs font-medium uppercase text-primary transition duration-150 ease-in-out focus:outline-none focus:ring-0"
             type="button"
             id="button-addon3"
             data-te-ripple-init
