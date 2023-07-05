a88d5d16408047e3905ee5848dd758085dcca637
Jakub <jak.malinowski94@gmail.com>
2023-07-05 13:41:14 +0200

    code cleanup

--- frontend/src/Components/Product.js
+++ frontend/src/Components/Product.js
@@ -18,7 +18,7 @@ const Product = ({ product }) => {
     }
   };
   return product.available ? (
-    <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+    <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow">
       <Link to={`/product/${product.id}`}>
         <img
           className="p-8 rounded-t-lg object-cover h-48 w-96"
@@ -28,7 +28,7 @@ const Product = ({ product }) => {
       </Link>
       <div className="px-5 pb-5">
         <Link to={`/product/${product.id}`}>
-          <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
+          <h5 className="text-xl font-semibold tracking-tight text-gray-900 ">
             {product.name}
           </h5>
         </Link>
@@ -39,12 +39,12 @@ const Product = ({ product }) => {
           {product.brand.name}
         </div>
         <div className="flex items-center justify-between">
-          <span className="text-3xl font-bold text-gray-900 dark:text-white">
+          <span className="text-3xl font-bold text-gray-900">
             {product.price}zł
           </span>
           <button
             href="#"
-            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
             onClick={() => handleAddToCart(product.id)}
           >
             Dodaj do koszyka
--- frontend/src/Components/ProductDetail.js
+++ frontend/src/Components/ProductDetail.js
@@ -119,39 +119,8 @@ const ProductDetail = () => {
                 {product.description}
               </div>
 
-              {/* <div className="flex mt-6 items-center pb-5 border-b-2 border-gray-200 mb-5">
-                <div className="flex">
-                  <span className="mr-3">Color</span>
-                  <button className="border-2 border-gray-300 rounded-full w-6 h-6 focus:outline-none"></button>
-                  <button className="border-2 border-gray-300 ml-1 bg-gray-700 rounded-full w-6 h-6 focus:outline-none"></button>
-                  <button className="border-2 border-gray-300 ml-1 bg-red-500 rounded-full w-6 h-6 focus:outline-none"></button>
-                </div>
-                <div className="flex ml-6 items-center">
-                  <span className="mr-3">Size</span>
-                  <div className="relative">
-                    <select className="rounded border appearance-none border-gray-400 py-2 focus:outline-none focus:border-red-500 text-base pl-3 pr-10">
-                      <option>SM</option>
-                      <option>M</option>
-                      <option>L</option>
-                      <option>XL</option>
-                    </select>
-                    <span className="absolute right-0 top-0 h-full w-10 text-center text-gray-600 pointer-events-none flex items-center justify-center">
-                      <svg
-                        fill="none"
-                        stroke="currentColor"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="2"
-                        className="w-4 h-4"
-                        viewBox="0 0 24 24"
-                      >
-                        <path d="M6 9l6 6 6-6"></path>
-                      </svg>
-                    </span>
-                  </div>
-                </div>
-              </div> */}
-              <div className="flex justify-between align-middle">
+
+              <div className="flex justify-between align-middle mt-5">
                 <span className="title-font font-medium text-2xl text-gray-900">
                   {product.price} zł
                 </span>
