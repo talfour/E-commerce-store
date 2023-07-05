09a596d5fd4a83f792bcedbe6f4e1c1cc328b66a
Jakub <jak.malinowski94@gmail.com>
2023-07-05 19:36:18 +0200

    Order listing for user

---
+++ frontend/src/Components/Order.js
@@ -0,0 +1,82 @@
+import React from "react";
+import defaultImage from "../assets/thenounproject.svg"
+import x_mark from "../assets/x_mark.svg"
+import check_mark from "../assets/check_mark.svg";
+import { Link } from "react-router-dom";
+
+const Order = ({ orders }) => {
+    const formatDate = (dateString) => {
+        const options = {
+          year: "numeric",
+          month: "long",
+          day: "numeric",
+        };
+        return new Date(dateString).toLocaleDateString(undefined, options)
+    }
+  return (
+    <div className="flex flex-col p-5 justify-center align-middle ">
+      {orders.map((order) => (
+        <div
+          key={order.id}
+          className="m-auto mb-5 p-5 w-[50vw] bg-white border border-gray-200 rounded-lg shadow"
+        >
+          <div className="flex justify-between">
+            <span className="italic">Zamówienie #{order.id}</span>
+            <span className="italic"> {formatDate(order.created)}</span>
+          </div>
+          <div className="flex">
+            <p>Opłacone:</p>{" "}
+            {order.paid ? (
+              <img className="w-5" alt="tak" src={check_mark} />
+            ) : (
+              <img className="w-5" alt="nie" src={x_mark} />
+            )}
+          </div>
+          <div className="flex justify-between my-5 items-center">
+            <p>
+              Imię i Nazwisko: {order.first_name} {order.last_name}
+            </p>
+            <div>
+              <p className="text-right">Adres dostawy:</p>
+              <div className="text-right">
+                <p>
+                  {order.address} {order.address2}
+                </p>
+                <p>
+                  {order.post_code} {order.city}
+                </p>
+              </div>
+            </div>
+          </div>
+          {order.items.map((item) => (
+            <div key={item.id} className="flex items-center mb-5">
+              <img
+                alt={item.product.name}
+                className="w-20"
+                src={
+                  item.product?.images[0]?.image
+                    ? item.product?.images[0]?.image
+                    : defaultImage
+                }
+              />
+              <div className="pl-5">
+                <Link
+                  className="text-blue-500 hover:text-blue-700"
+                  to={`/product/${item.product.id}`}
+                >
+                  {item.product.name}
+                </Link>
+                <p>Cena: {item.price}zł</p>
+                <p>Ilość: {item.quantity}</p>
+                <p>Łącznie: {(item.price * item.quantity).toFixed(2)}zł</p>{" "}
+              </div>
+            </div>
+          ))}
+          <p>Całkowity koszt: {order.total_cost}zł</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Order;
--- frontend/src/Components/Orders.js
+++ frontend/src/Components/Orders.js
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import { axiosInstance } from '../axios';
+import Order from './Order';
+
 
 const Orders = () => {
+
+  const [orders, setOrders] = useState([]);
+  const getOrderData = async () => {
+    const response = await axiosInstance.get('order/')
+    if (response.status === 200) {
+      console.log(response.data);
+      setOrders(response.data)
+    }
+  }
+  useEffect(() => {
+    getOrderData()
+  }, [])
+
   return (
-    <div>Orders</div>
+    <div>
+      <h1 className="text-center lg:text-5xl text-3xl pt-10 pb-10 font-roboto font-bold">Twoje zamówienia</h1>
+      <div>
+        <Order orders={orders}></Order>
+      </div>
+    </div>
   )
 }
 
